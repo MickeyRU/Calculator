1b6ee0ab38ccb0ffb18ed212398638a2a6011e9d
Pavel Afanasiev <afanasyevpv9@gmail.com>
2022-10-17 16:58:18 +0300

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Павел Афанасьев on 17.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
